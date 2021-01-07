5640cd7a827f8847bfc7408c6fc24091200983db
Joshua Haddock <joshhaddock88@gmail.com>
2021-01-07 13:55:19 -0800

    Make hover change div class to have background as black

--- script.js
+++ script.js
@@ -3,12 +3,21 @@
 //for each number create a div
 const container = document.getElementById('container');
 
-function createColumns(number) {
+function createGrid(number) {
     const gridSize = number**2;
     for(i=0; i < gridSize; i++) {
         let square = document.createElement('div');
         square.className = 'square';
-        square.textContent = '*';
         container.appendChild(square);
     }
+}
+window.onload = createGrid(35);
+
+
+const squares = container.getElementsByClassName('square');
+
+for(i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function() {
+        this.className += ' active';
+    });
 }
